c6b37868c2306be3d9f0cb6400314fa729a18d23
pritamawatade <pritamawatade5@gmail.com>
2025-04-07 07:41:11 +0530

    Typescript start

---
+++ week-12/Typescript/hello.js
@@ -0,0 +1,4 @@
+function add(x, y) {
+    return x + y;
+}
+var b = add(2, 4);
---
+++ week-12/Typescript/hello.ts
@@ -0,0 +1,16 @@
+interface User{
+    firstname:string,
+    lastname: string,
+    email: string
+}
+
+function add1(x: number, y:number){
+    return x+y
+}
+
+function createUser(user: User){
+    return 
+
+}
+
+let c = add1(2,4)
